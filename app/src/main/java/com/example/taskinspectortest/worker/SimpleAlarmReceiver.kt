40b0b869d572f1aea7110b698b442ed3ce9bd9bc
Hiroki-Yamabayashi <hiroki.yamabayashi@fuller.co.jp>
2025-10-16 15:45:27 +0900

    不要なToastを削除

--- app/src/main/java/com/example/taskinspectortest/worker/SimpleAlarmReceiver.kt
+++ app/src/main/java/com/example/taskinspectortest/worker/SimpleAlarmReceiver.kt
@@ -3,16 +3,15 @@ package com.example.taskinspectortest.worker
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import com.example.taskinspectortest.util.ToastController
+import android.util.Log
 
 // AlarmManager の BroadcastReceiver 実装
 class SimpleAlarmReceiver : BroadcastReceiver() {
-    private val tagName = "MyAlarm"
+    private val tag = "MyAlarm"
     override fun onReceive(context: Context?, intent: Intent?) {
-        val toastController = ToastController(context)
-        toastController.showLatestToast("Alarm received. Performing immediate task...")
+        Log.d(tag, "Alarm received. Performing immediate task...")
         // アラームは短いタスクにのみ使用すべき
-        toastController.showLatestToast("Alarm task finished.")
+        Log.d(tag, "Alarm task finished.")
         // Wakelock を取得する場合は、onReceive() の終了時に解放されるように注意
     }
 }
--- app/src/main/java/com/example/taskinspectortest/worker/SimpleWorker.kt
+++ app/src/main/java/com/example/taskinspectortest/worker/SimpleWorker.kt
@@ -1,19 +1,18 @@
 package com.example.taskinspectortest.worker
 
 import android.content.Context
+import android.util.Log
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
-import com.example.taskinspectortest.util.ToastController
 import kotlinx.coroutines.delay
 
 // WorkManager の Worker 実装
 class SimpleWorker(context: Context, params: WorkerParameters) : CoroutineWorker(context, params) {
-    val toastController = ToastController(context)
+    val tag = "MyWorker"
     override suspend fun doWork(): Result {
-        "MyWorker"
-        toastController.showLatestToast(message = "Worker started. Doing some background work...")
+        Log.d(tag, "Worker started. Doing some background work...")
         delay(3000) // 3秒間処理をシミュレート
-        toastController.showLatestToast(message = "Worker finished.")
+        Log.d(tag, "Worker finished.")
         return Result.success()
     }
 }
